fbfc891f14bce762d566ab8b15f0085ad47678d2
Hawkurane <hawkurane@gmail.com>
2018-10-15 17:19:05 +0200

    Generateur de Cle de Session

--- src/main/java/tools/Logger.java
+++ src/main/java/tools/Logger.java
@@ -1,6 +1,12 @@
 package tools;
 
+import java.util.Random;
+
 public class Logger {
+	
+	final static String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	final static int N = alphabet.length();
+	final static int KEY_LENGTH = 64;
 
 	public static boolean logIn(String username, String password) {
 		//TODO
@@ -10,6 +16,15 @@ public class Logger {
 	
 	public static boolean logOut(String username) {
 		return true;
+	}
+	
+	public static String generateSessionKey() {
+		Random rand = new Random();
+		StringBuilder str = new StringBuilder();
+		for(int i = 0; i<KEY_LENGTH; i++) {
+			str.append(alphabet.charAt(rand.nextInt(N)));
+		}
+		return str.toString();
 	}
 	
 	
