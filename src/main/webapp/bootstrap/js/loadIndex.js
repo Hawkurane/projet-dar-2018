e238ab605054d0697ac24f06660696931c4e9251
Hawkurane <hawkurane@gmail.com>
2018-11-20 10:58:41 +0100

    fix typo sustring > substring

--- src/main/webapp/bootstrap/js/loadIndex.js
+++ src/main/webapp/bootstrap/js/loadIndex.js
@@ -25,7 +25,7 @@ $.get(
                 <div class="row" style="margin-bottom: 10px; text-align: center">
                     <div class="card text-white bg-dark mb3" style="width: 100%">
                         <div class="card-header ${color} mb-3">
-                            [${element.league}] Day n°${element.matchDay}, ${(element.time).sustring(1,6)} (${element.status})
+                            [${element.league}] Day n°${element.matchDay}, ${(element.time).subtring(1,6)} (${element.status})
                         </div>
                         <div class="card-body">
                             <div class="media">
